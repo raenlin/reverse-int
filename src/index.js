7d5acc7f85a13b5682fd91fc2c1f185189722284
Darya Pchelintseva <raelin@mail.ru>
2023-08-01 21:30:09 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  const nStr = String(n);
+
+  let result = '';
+
+  for(let i=0; i<nStr.length; i++) {
+    result = `${nStr[i]}${result}`;
+  }
+
+  result = Number(result);
+
+  return result;  
 }
